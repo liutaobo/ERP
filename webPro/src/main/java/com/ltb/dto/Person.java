9470faa29af462cac8dbe1a0455cb226a66d3cfc
Administrator <Administrator@WIN-CU33SJAJBQ0>
2021-07-19 10:55:51 +0800

    new function

--- webPro/src/main/java/com/ltb/dto/Person.java
+++ webPro/src/main/java/com/ltb/dto/Person.java
@@ -1,9 +1,12 @@
 package com.ltb.dto;
 
+import java.io.InputStream;
+
 import org.springframework.web.servlet.DispatcherServlet;
 
 public class Person {
 	public static void main(String[] args) {
 		DispatcherServlet a = null;
+		InputStream is;
 	}
 }

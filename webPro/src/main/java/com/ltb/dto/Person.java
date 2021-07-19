047eec646d02d965848c901354f5fc465a395482
Administrator <Administrator@WIN-CU33SJAJBQ0>
2021-07-19 09:28:48 +0800

    项目搭建

---
+++ webPro/src/main/java/com/ltb/dto/Person.java
@@ -0,0 +1,9 @@
+package com.ltb.dto;
+
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class Person {
+	public static void main(String[] args) {
+		DispatcherServlet a;
+	}
+}
---
+++ webPro/src/main/java/webPro/App.java
@@ -0,0 +1,14 @@
+package webPro;
+
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+@EnableWebMvc
+public class App {
+	public static void main(String[] args) {
+		ContextLoaderListener serblet;
+		AbstractAnnotationConfigDispatcherServletInitializer bean;
+	}
+	
+}
